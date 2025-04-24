b57540f5fa65c2638ba2849061fa11418b9249ae
JackDiSpade21 <alearci44@gmail.com>
2025-04-25 00:33:58 +0200

    spotify completo

--- mhw3.js
+++ mhw3.js
@@ -1,17 +1,40 @@
 const api_ticketmaster = '';
 const api_spotify = '';
+const secret_spotify = '';
+let spotify_token = '';
+
+document.addEventListener("click", closeSpotifyBox);
+const modal_spotify = document.querySelector("#spotifybox");
+const spotify_songs = document.querySelector("#spotify-songs");
+const spotifyTitle = document.querySelector("#spotify-title");
+const spotifyPic = document.querySelector("#spotify-pic");
+const closeButtonSpotify = document.querySelector("#close-button-spotify");
+closeButtonSpotify.addEventListener("click", closeButtonSpotifyBox);
 
 fetch('./keys.txt')
     .then(response => response.text(), onError)
     .then(text => {
         const api_ticketmaster = text.split('\n')[0].trim();
         const api_spotify = text.split('\n')[1].trim();
+        const secret_spotify = text.split('\n')[2].trim();
         console.log('API Key spotify:', api_spotify);
+        console.log('Secret Spotify:', secret_spotify);
         console.log('API Key ticketmaster:', api_ticketmaster);
 
         fetch('https://app.ticketmaster.com/discovery/v2/events.json' + '?apikey=' + api_ticketmaster + '&locale=it-it' + '&sort=id,asc' + '&size=50')
             .then(onResponseTicket, onError)
             .then(onJSONTicket);
+
+        
+        fetch("https://accounts.spotify.com/api/token",{
+                method: "post",
+                body: 'grant_type=client_credentials',
+                headers:
+                {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'Authorization': 'Basic ' + btoa(api_spotify + ':' + secret_spotify)
+                }
+            }).then(onTokenResponse, onError).then(onTokenJson);
     })
 
 function onResponseTicket(response) {
@@ -156,5 +179,143 @@ function createCard(name, type, imageUrl, section, classes) {
         article.appendChild(span);
     }
 
+    if(type === 'Musica') {
+        article.addEventListener('click', openSpotifyBox);
+    }
+
     return;
+}
+
+function onTokenResponse(response) {
+    if (response.ok) {
+        return response.json();
+    }
+    else
+        return null;
+}
+
+function onTokenJson(json) {
+    console.log(json);
+    spotify_token = json.access_token;
+}
+
+
+function openSpotifyBox(event) {
+
+    if(!modal_spotify.classList.contains('hidden')) {
+        return;
+    }
+
+    const name = event.currentTarget.querySelector('h3').textContent;
+
+    modal_spotify.classList.remove('hidden');
+    document.body.classList.add("no-scroll");
+    event.stopPropagation();
+
+    fetch("https://api.spotify.com/v1/search?q=" + name + "&type=artist", {
+        headers: {
+            'Authorization': 'Bearer ' + spotify_token
+        }
+    }).then(onResponseArtist, onError).then(onJSONArtist);
+
+
+    spotifyPic.src = './icons/person.png';
+    spotifyTitle.innerHTML = 'Brani popolari di <br>' + name;
+}
+
+function onResponseArtist(response) {
+    if (response.ok) {
+        return response.json();
+    }
+    else
+        return null;
+}
+
+function onJSONArtist(json) {
+    console.log(json);
+    const artistId = json.artists.items[0].id;
+    const artistImg = json.artists.items[0].images[0].url;
+    spotifyPic.src = artistImg;
+
+    fetch("https://api.spotify.com/v1/artists/" + artistId + "/top-tracks?market=IT", {
+        headers: {
+            'Authorization': 'Bearer ' + spotify_token
+        }
+    }).then(onResponseTopTracks, onError).then(onJSONTopTracks);
+
+}
+
+function onResponseTopTracks(response) {
+    if (response.ok) {
+        return response.json();
+    }
+    else
+        return null;
+}
+
+function onJSONTopTracks(json) {
+    console.log(json);
+    const tracks = json.tracks;
+
+    for (let i = 0; i < 3; i++) {
+        const track = tracks[i];
+        const canzone = track.name;
+        const album = track.album.name;
+        const url = track.external_urls.spotify;
+        let imageUrl = '';
+
+        for (const image of track.album.images) {
+            if (image.height > 500) {
+                imageUrl = image.url;
+                break;
+            }
+        }
+
+        createSongBox(canzone, album, imageUrl, url);
+    }
+
+}
+
+function createSongBox(canzone, album, image, url) {
+
+    const songbox = document.createElement('div');
+    songbox.classList.add('songbox');
+    const img = document.createElement('img');
+    img.src = image;
+    songbox.appendChild(img);
+    const songInfo = document.createElement('div');
+    songInfo.classList.add('song-info');
+    const songName = document.createElement('p');
+    songName.textContent = canzone
+    songInfo.appendChild(songName);
+    const songAlbum = document.createElement('p');
+    songAlbum.textContent = album;
+    songInfo.appendChild(songAlbum);
+    songbox.appendChild(songInfo);
+    const playButton = document.createElement('a');
+    playButton.href = url;
+    const playImg = document.createElement('img');
+    playImg.src = './icons/play-button-spot.png';
+    playButton.appendChild(playImg);
+    songbox.appendChild(playButton);
+
+    spotify_songs.appendChild(songbox);
+    
+    return;
+}
+
+function closeSpotifyBox(event) {
+    if (modal_spotify.contains(event.target)) {
+        return;
+    }
+
+    spotify_songs.innerHTML = "";
+    modal_spotify.classList.add('hidden');
+    document.body.classList.remove("no-scroll");
+}
+
+function closeButtonSpotifyBox(){
+    spotify_songs.innerHTML = "";
+    modal_spotify.classList.add('hidden');
+    document.body.classList.remove("no-scroll");
 }
